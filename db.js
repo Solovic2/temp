47c1f42495b4c20e11ffbcd0333394f062305cc6
Solovic2 <islammohamed323@gmail.com>
2023-06-12 13:25:32 +0300

    ORM Functions

--- db.js
+++ db.js
@@ -1,16 +1,33 @@
 const oracledb = require('oracledb');
 
-async function executeQuery(sql, binds = []) {
+// Define the database configuration parameters
+const dbConfig = {
+  user: 'complain',
+  password: '123',
+  connectString: '62.117.51.155:1521/xe',
+};
+
+// Establish a database connection
+async function connect() {
   let connection;
-  const config = {
-    user: 'complainments',
-    password: '123',
-    connectString: '62.117.51.154:1521/xe',
-  };
   try {
-    connection = await oracledb.getConnection(config);
-    const result = await connection.execute(sql, binds, { outFormat: oracledb.OBJECT });
-    return result.rows;
+    connection = await oracledb.getConnection(dbConfig);
+    console.log('Connected to database');
+    return connection;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// Function to add data to the database
+async function addData(path, info) {
+  let connection;
+  try {
+    connection = await connect();
+    const insertSql = `INSERT INTO FILES (PATH, INFO) VALUES (:1, :2)`;
+    await connection.execute(insertSql, [path, info]);
+    await connection.commit();
+    console.log('Data added to database');
   } catch (err) {
     console.error(err);
   } finally {
@@ -24,4 +41,48 @@ async function executeQuery(sql, binds = []) {
   }
 }
 
-module.exports = executeQuery;
+// Function to update data in the database
+async function updateData(path, info, datetime, id) {
+  let connection;
+  try {
+    connection = await connect();
+    const updateSql = `UPDATE FILES SET PATH = :1, INFO = :2, DATETIME = :3 WHERE ID = :4`;
+    await connection.execute(updateSql, [path, info, datetime, id]);
+    await connection.commit();
+    console.log('Data updated in database');
+  } catch (err) {
+    console.error(err);
+  } finally {
+    if (connection) {
+      try {
+        await connection.close();
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }
+}
+
+// Function to delete data from the database
+async function deleteData(id) {
+  let connection;
+  try {
+    connection = await connect();
+    const deleteSql = `DELETE FROM FILES WHERE ID = :1`;
+    await connection.execute(deleteSql, [id]);
+    await connection.commit();
+    console.log('Data deleted from database');
+  } catch (err) {
+    console.error(err);
+  } finally {
+    if (connection) {
+      try {
+        await connection.close();
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }
+}
+
+module.exports = { connect, addData, updateData, deleteData };
--- index.js
+++ index.js
@@ -1,22 +1,45 @@
 const express = require('express');
+const fs = require('fs');
 const oracledb = require('oracledb');
-const executeQuery = require('./db');
+const chokidar = require('chokidar');
+const database = require('./db');
+const folderPath = "E:\\Islam\\temp";
 const app = express();
 
-async function getUsers() {
-  const sql = 'SELECT * FROM files';
-  const users = await executeQuery(sql);
-  console.log(users);
-}
 
+function readFiles() {
+  fs.readdir(folderPath, (err, files) => {
+    if (err) {
+      console.error(err);
+    } else {
+      files.forEach((file) => {
+        console.log(file);
+      });
+    }
+  });
+}
+readFiles()
 // create a route to test the database connection
 app.get('/', (req, res) => {
-  
-    res.send(getUsers())
-
+    
+    res.send("Hello")
 });
 
+
+// const watcher = chokidar.watch(folderPath, {
+//   persistent: true,
+//   ignoreInitial: true,
+// });
+// watcher.on('all', (event, path) => {
+//   console.log(event, path);
+//   readFiles();
+// });
+// watcher
+//   .on('add', path => console.log(`File ${path} has been added`))
+//   .on('change', path => console.log(`File ${path} has been changed`))
+//   .on('unlink', path => console.log(`File ${path} has been removed`))
+//   .on('error', error => console.log(`Watcher error: ${error}`));
 // start the server
 app.listen(9000, () => {
-  console.log('Server started on port 3000');
+  console.log('Server started on port 9000');
 });
